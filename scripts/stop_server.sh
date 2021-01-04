fc1a01ed7b71e7cb74d1d5d959d86cc243e7e363
yg57404 <66639489+yg57404@users.noreply.github.com>
2021-01-04 18:38:32 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 isExistApp=`pgrep apache2`
 if [[ -n  $isExistApp ]]; then
-   systemctl stop apache2
+   systemctl restart apache2
 fi
 isExistApp=`pgrep mysql`
 if [[ -n  $isExistApp ]]; then
-    systemctl stop mysql
+    systemctl restart mysql
 fi
